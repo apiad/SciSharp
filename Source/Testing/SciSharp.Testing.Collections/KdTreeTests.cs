a6f1555cfc46e8e9722df3a0401ff75770ec7590
AlejandroPiad <alepiad@gmail.com>
2013-05-09 11:46:39 -0400

    Added empty KdTree tests

---
+++ Source/Testing/SciSharp.Testing.Collections/KdTreeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SciSharp.Testing.Collections
+{
+    [TestClass]
+    public class KdTreeTests
+    {
+        [TestMethod]
+        public void ReturnTheClosestPoints()
+        {
+        }
+    }
+}
